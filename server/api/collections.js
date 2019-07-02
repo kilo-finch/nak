f91e8eec1adc1b33294fc64c0693ad139a042208
Cristian Martinez <2cristianmartinez@gmail.com>
2019-07-02 15:08:43 -0400

    add link to collection route

--- server/api/collections.js
+++ server/api/collections.js
@@ -19,3 +19,32 @@ router.get('/', async (req, res, next) => {
     res.sendStatus(403)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    console.log('req user', req.user)
+    const userId = req.user.id
+    const {description, title, url, favicon, orderId} = req.body
+
+    const userTeam = await Team.findOne({
+      through: {where: userId}
+    })
+    console.log('userTeam', userTeam)
+
+    const userCollection = await Collection.findOne({
+      where: {teamId: +userTeam.id}
+    })
+    const link = {
+      description,
+      title,
+      url,
+      favicon,
+      collectionId: userCollection.id,
+      orderId
+    }
+    const newLink = await Links.create(link)
+    res.send(newLink)
+  } catch (error) {
+    next(error)
+  }
+})
--- server/index.js
+++ server/index.js
@@ -105,7 +105,7 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-// const syncDb = () => db.sync({force: true})
+// const syncDb = () => db.sync({ force: true })
 const syncDb = () => db.sync()
 
 async function bootApp() {
