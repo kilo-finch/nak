b7e7b47f9e81104f036fc5ca10cab87a4f35e8d8
Nikita.Girko <girkonv@gmail.com>
2019-07-02 11:29:18 -0400

    if there is no req.user send status 403

--- server/api/collections.js
+++ server/api/collections.js
@@ -3,26 +3,19 @@ const {Collection, Links, Team, User} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  try {
-    const allCollectionsFromAllTeams = await User.findAll({
-      where: {
-        id: +req.user.id
-      },
-      include: {model: Team, include: {model: Collection, include: Links}}
-    })
-    // const allCollectionsFromAllTeams = await Team.findAll({
-    //   include: {model: Collection, include: Links}
-    // })
-    // const user = await User.findByPk(1)
-    // const userTeam = await user.getTeams(null, {
-    //   include: [{model: Collection, include: {model: Links}}]
-    // })
-    // const userTeamCollections = await Promise.all(
-    //   userTeam.map(async team => team.getCollections())
-    // )
-    // res.send(userTeamCollections)
-    res.send(allCollectionsFromAllTeams)
-  } catch (error) {
-    next(error)
+  if (req.user) {
+    try {
+      const allCollectionsFromAllTeams = await User.findAll({
+        where: {
+          id: +req.user.id
+        },
+        include: {model: Team, include: {model: Collection, include: Links}}
+      })
+      res.send(allCollectionsFromAllTeams)
+    } catch (error) {
+      next(error)
+    }
+  } else {
+    res.sendStatus(403)
   }
 })
