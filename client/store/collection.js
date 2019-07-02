819d072265d8ad99594405318cfdc65c1c020ce3
Eric K <kreitere@gmail.com>
2019-07-02 11:36:25 -0400

    feature: added link store, created DRY util components, added basic styling and an add link button to user page with a popup

---
+++ client/store/collection.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * INITIAL STATE
+ */
+
+/**
+ * ACTION TYPES
+ */
+
+/**
+ * ACTION CREATORS
+ */
+
+/**
+ * THUNK CREATORS
+ */
+
+/**
+ * REDUCER
+ */
--- client/store/user.js
+++ client/store/user.js
@@ -2,15 +2,15 @@ import axios from 'axios'
 import history from '../history'
 
 /**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
  * ACTION TYPES
  */
 const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
-
-/**
- * INITIAL STATE
- */
-const defaultUser = {}
 
 /**
  * ACTION CREATORS
