e8c1951383fbfe8a157f7b8da072e59ac2c878fe
Eric K <kreitere@gmail.com>
2019-07-08 17:49:02 -0400

    Update: fixed redirects, routes and landing page options

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -12,7 +12,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+          {/* <Link to="/home">Home</Link> */}
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -20,8 +20,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
+          {/* <Link to="/login">Login</Link> */}
+          {/* <Link to="/signup">Sign Up</Link> */}
         </div>
       )}
     </nav>
