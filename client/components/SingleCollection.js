fa21152d17870e2a54c59e0a762b35435064addc
Nikita.Girko <girkonv@gmail.com>
2019-07-09 10:45:30 -0400

    singlecollection class->function, remove some comments

--- client/components/SingleCollection.js
+++ client/components/SingleCollection.js
@@ -5,39 +5,30 @@ import {connect} from 'react-redux'
 import HTML5Backend from 'react-dnd-html5-backend'
 import {DndProvider} from 'react-dnd'
 
-class SingleCollection extends Component {
-  // moveCard = (dragIndex, hoverIndex) => {
-  //   let links = [...this.state.links]
-  //   let temp = links[dragIndex]
-  //   links.splice(dragIndex, 1)
-  //   links.splice(hoverIndex, 0, temp)
-  // }
-
-  render() {
-    const {collection} = this.props
-    return (
-      <DndProvider backend={HTML5Backend}>
-        <div
-          style={{border: '2px black solid', height: '400px'}}
-          className="container"
-        >
-          <div className="field is-grouped is-grouped-multiline">
-            {collection.links ? (
-              collection.links.map((link, index) => (
-                <LinkCard link={link} index={index} key={link.id} />
-              ))
-            ) : (
-              <h3>
-                You have no links saved in your collection! Add a link to get
-                started.
-              </h3>
-            )}
-          </div>
-          {/* <LinkForm /> */}
+const SingleCollection = props => {
+  const {collection} = props
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div
+        style={{border: '2px black solid', height: '400px'}}
+        className="container"
+      >
+        <div className="field is-grouped is-grouped-multiline">
+          {collection.links ? (
+            collection.links.map((link, index) => (
+              <LinkCard link={link} index={index} key={link.id} />
+            ))
+          ) : (
+            <h3>
+              You have no links saved in your collection! Add a link to get
+              started.
+            </h3>
+          )}
         </div>
-      </DndProvider>
-    )
-  }
+        {/* <LinkForm /> */}
+      </div>
+    </DndProvider>
+  )
 }
 
 export default SingleCollection
