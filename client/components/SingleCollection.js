db612f28c09c7f53acf4567bfeed5c3e5d651df9
Cristian Martinez <2cristianmartinez@gmail.com>
2019-07-11 17:59:27 -0400

    feature: open all links button added to menu bar fix

--- client/components/SingleCollection.js
+++ client/components/SingleCollection.js
@@ -4,14 +4,6 @@ import {LinkCard, LinkForm} from './index'
 import HTML5Backend from 'react-dnd-html5-backend'
 import {DndProvider} from 'react-dnd'
 
-// const openAll = (event, links) => {
-//   links.forEach(link => {
-//     event.preventDefault()
-//     window.open(link.url, '_blank')
-//   })
-//   window.focus()
-// }
-
 const SingleCollection = props => {
   const {collection} = props
   return (
@@ -19,13 +11,7 @@ const SingleCollection = props => {
       <div>
         <div>
           {collection.links ? (
-            <div
-            // className="field is-grouped is-grouped-multiline"
-            // style={{
-            //   padding: '5px',
-            //   position: 'relative',
-            // }}
-            >
+            <div>
               {collection.links.length > 0 ? (
                 <div
                   className="field is-grouped is-grouped-multiline"
@@ -37,14 +23,6 @@ const SingleCollection = props => {
                   {collection.links.map((link, index) => (
                     <LinkCard link={link} index={index} key={link.id} />
                   ))}
-                  {/* <button
-                    type="button"
-                    onClick={() => openAll(event, collection.links)}
-                    className="button is-primary is-primary is-outlined is-small"
-                    style={{ position: 'absolute', right: '0', bottom: '0' }}
-                  >
-                    Open All
-                  </button> */}
                 </div>
               ) : (
                 <h3>
